bc45341b3d64838cf20ac276e6654f4a17f6ee4e
Marcin Ziółkowski <cotampanie@gmail.com>
2015-05-12 21:25:17 +0200

    fix the redirection

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,8 @@ class MainController < ApplicationController
   def index
     if current_user
       redirect_to user_movies_path(current_user)
+    else
+      redirect_to log_in_path
     end
   end
 
